1c0a2d9488b1bd60a5116e535a0bab9368c79a2b
Ding Liu <contact@dingliu.me>
2025-04-12 21:53:18 -0400

    dot source regular posh profile to VS code one

---
+++ powershell/Microsoft.VSCode_profile.ps1
@@ -0,0 +1,5 @@
+# This file is used to customize the PowerShell profile for Visual Studio Code.
+# It is loaded when starting a new PowerShell session in VS Code.
+
+# Load the regular PowerShell profile for Visual Studio Code
+. "$([Environment]::GetFolderPath('MyDocuments'))\PowerShell\Microsoft.PowerShell_profile.ps1"
