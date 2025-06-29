86735d529c6f566bddc84019e067280864be1c65
Ding Liu <contact@dingliu.me>
2025-06-29 09:51:48 -0400

    Add comments to PowerShell profile

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,9 +1,18 @@
-# This is the PowerShell profile script that runs every time a new PowerShell session is started.
-# It sets up the environment for PowerShell, including loading modules and configuring the prompt.
+# =============================================================================
+# This is the PowerShell profile script that runs every time a new PowerShell
+# session is started.
+#
+# It sets up the environment for PowerShell, including loading modules and
+# configuring the prompt.
+#
+# To use this file, create a symlink to it in your PowerShell profile path.
+# For example, you can run the following command in PowerShell:
+#   New-Item -ItemType SymbolicLink -Path $PROFILE -Target <path to this file>
+# =============================================================================
 
-########################################################################
+# =============================================================================
 # History search
-########################################################################
+# =============================================================================
 # Partial command history search
 Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
