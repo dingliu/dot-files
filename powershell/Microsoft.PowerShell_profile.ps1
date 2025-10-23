1c55d5369c9a1061b00e31bfd06d9da6e6f607eb
Ding Liu <contact@dingliu.me>
2025-10-23 19:22:16 -0400

    feat: add ChocolateyProfile to PS profile

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -19,3 +19,13 @@ Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
 
 # Initialize oh-my-posh
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/negligible.omp.json" | Invoke-Expression
+
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
